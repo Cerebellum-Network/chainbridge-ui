e287010077b9c1bc0244659e39fc185a006b59c7
Andrei Navoichyk <andrei@cere.io>
2022-04-20 19:36:15 +0400

    updated Cere's rpc url

--- config/chainbridge-runtime-config.cere.dev.js
+++ config/chainbridge-runtime-config.cere.dev.js
@@ -38,7 +38,7 @@ window.__RUNTIME_CONFIG__ = {
         networkId: 2,
         name: "Cere Devnet",
         decimals: cereTokenDecimals,
-        rpcUrl: "wss://rpc.devnet.cere.network:9945",
+        rpcUrl: "wss://rpc.v2.qanet.cere.network/ws",
         type: "Substrate",
         nativeTokenSymbol: "CERE",
         availableAsHomeNetwork: true,
