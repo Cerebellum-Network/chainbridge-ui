c4a0bd20413ce239ac8bc0745d0d8382f25bc082
shamilkhan <shamilkhan1999@gmail.com>
2023-08-21 16:28:31 +0400

    Remove styles from AddressSelectInput

--- src/Components/Custom/AddressSelectInput.tsx
+++ src/Components/Custom/AddressSelectInput.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 import {SelectInput} from "@chainsafe/common-components";
 import {useField} from "formik";
-import {createStyles, ITheme, makeStyles} from "@chainsafe/common-theme";
 import {DestinationChainContext} from "../../Contexts/Adaptors/interfaces";
-
-const useStyles = makeStyles(({constants}: ITheme) =>
-    createStyles({
-        root: {
-            margin: "24px 0px"
-        },
-    })
-);
 
 interface IAddressSelectInput {
     addresses: DestinationChainContext["addresses"];
@@ -22,20 +13,18 @@ const AddressSelectInput: React.FC<IAddressSelectInput> =
          addresses,
          name,
      }) => {
-        const classes = useStyles();
         const [field, meta, helpers] = useField(name);
 
         return (
-            <div className={classes.root}>
-                <SelectInput
-                    label="Select a destination account"
-                    options={addresses}
-                    onChange={(address) => {
-                        helpers.setValue(address);
-                    }}
-                    placeholder="Select a destination account"
-                />
-            </div>
+            <SelectInput
+                value={field.value}
+                label="Select a destination account"
+                options={addresses}
+                onChange={(address) => {
+                    helpers.setValue(address);
+                }}
+                placeholder="Select a destination account"
+            />
         );
     };
 
